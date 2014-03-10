a6c2c7314bc1bf75db96daaf04ccc8403403b56d
Jo Cranford <jo.cranford@gmail.com>
2014-03-10 16:33:49 +1100

    Added initial spec for typeform class

---
+++ spec/typeform_spec.rb
@@ -0,0 +1,35 @@
+# encoding: UTF-8
+
+require 'typeform'
+
+describe TypeForm do
+  let(:form_id) { '1234' }
+  let(:api_key) { 'some api key' }
+
+  let!(:data_receiver) do
+    data_receiver = double(DataReceiver)
+    allow(DataReceiver).to receive(:new).and_return(data_receiver)
+    data_receiver
+  end
+
+  context 'no SSL config' do
+  
+    it 'creates a new DataReceiver with the correct form_id and api_key' do
+      TypeForm.new.get form_id, api_key
+      expect(DataReceiver).to have_received(:new).with hash_including(form_id: form_id, api_key: api_key)
+    end
+
+    xit 'sets SSL to false if it is not passed in' do
+    end
+
+  end
+
+  context 'with SSL config' do
+    xit 'passes SSL to the data receiver config if it is passed in' do
+    end
+  end
+
+  xit 'returns the results from the DataReceiver' do
+  end
+
+end
