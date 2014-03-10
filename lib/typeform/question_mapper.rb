9f528c6ab7dfd13ee486afb1bc9da87cacbea550
Jo Cranford <jo.cranford@gmail.com>
2014-03-10 18:42:44 +1100

    Added question mapper

---
+++ lib/typeform/question_mapper.rb
@@ -0,0 +1,25 @@
+# encoding: UTF-8
+
+module TypeForm
+
+  class QuestionMapper
+
+    def initialize typeform_questions
+      @questions = typeform_questions.map do |question_properties|
+        [ normalize(question_properties[:question]), question_properties[:id]]
+      end.to_h
+    end
+
+    def find question_text
+      @questions[normalize(question_text)]
+    end
+
+    private
+
+    def normalize text
+      text.upcase.strip
+    end
+
+  end
+
+end
---
+++ spec/typeform/question_mapper_spec.rb
@@ -0,0 +1,34 @@
+# encoding: UTF-8
+
+require 'typeform/question_mapper'
+
+module TypeForm
+
+  describe QuestionMapper do
+
+    let(:typeform_questions) do
+      [
+        { id: 'textfield1', question: 'what is your name?' },
+        { id: 'textfield2', question: 'what is your company?' }
+      ]
+    end
+
+    let(:question_mapper) { QuestionMapper.new(typeform_questions) }
+
+    it 'determines the field reference given the question text' do
+      expect(question_mapper.find "what is your name?").to eq "textfield1"
+    end
+
+    it 'ignores case' do
+      expect(question_mapper.find "WhAt is your COMPanY?").to eq "textfield2"
+    end
+
+    it 'ignores leading and trailing spaces' do
+    end
+
+    it 'ignores trailing question marks' do
+    end
+    
+  end
+
+end
